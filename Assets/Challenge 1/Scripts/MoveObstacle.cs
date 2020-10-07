93944839f8d88a6322063fcb7dadbc12b97a8ee0
ketrab2004 <bartek@fos.nl>
2020-10-07 14:25:30 +0200

    First Commit

---
+++ Assets/Challenge 1/Scripts/MoveObstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacle : MonoBehaviour
+{
+    public float rotSpeed = 45f;
+    public Vector3 rotAxis = Vector3.forward;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotAxis, rotSpeed*Time.deltaTime);
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlaneProp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneProp : MonoBehaviour
+{
+    public float propSpeed = 15f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime*propSpeed);
+    }
+}
